accefdf547c8d92f211adac17c67d67dab49e5b1
ansarimoiz979 <ansarimoiz979@gmail.com>
2023-04-04 15:54:30 +0530

    login screen with useref

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import Login from "./pages/home/login/Login";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+   <div>
+   <Routes>
+    {/* <Route exact path="/login" element={ user? <Navigate  to="/" />: <Login/>}/>
+     */}
+     <Route exact path="/" element={ <Login/> } />
+   </Routes>
+   </div>
+   </BrowserRouter>
   );
 }
 
---
+++ src/pages/home/login/Login.js
@@ -0,0 +1,67 @@
+import { useContext, useRef } from "react";
+import "./Login.css";
+import { CircularProgress } from "@material-ui/core";
+
+export default function Login() {
+  const email = useRef();
+  const password = useRef();
+//   const { user,isFetching, dispatch ,error} = useContext(AuthContext);
+
+//   const handleClick = (e) => {
+//     e.preventDefault();
+//     loginCall(
+//       { email: email.current.value, password: password.current.value },
+//       dispatch
+//     );
+
+//   };
+
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Connect-In</h3>
+          <span className="loginDesc">let's connect to success</span>
+        </div>
+        <div className="loginRight">
+          {/* <form className="loginBox" onSubmit={handleClick}>
+           */}
+                     <form className="loginBox">
+            <input
+              placeholder="Email"
+              type="email"
+              required
+              className="loginInput"
+              ref={email}
+            />
+            <input
+              placeholder="Password"
+              type="password"
+              required
+              minLength="6"
+              className="loginInput"
+              ref={password}
+            />
+            <button className="loginButton" type="submit" >
+              {/* {isFetching ? (
+                <CircularProgress color="white" size="20px" />
+                
+              ) : (
+                "Log In"
+              )} */}
+              Log In
+            </button>
+            <span className="loginForgot">Forgot Password?</span>
+            <button className="loginRegisterButton">
+              {/* {isFetching ? (
+                <CircularProgress color="white" size="20px" />
+              ) : (
+                "Create a New Account"
+              )} */}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
