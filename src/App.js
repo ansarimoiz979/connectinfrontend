991fe4b33ec528de1302b8c289bb1024221b5e1b
ansarimoiz979 <ansarimoiz979@gmail.com>
2023-04-11 19:46:39 +0530

    correction in app routing file and formate files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 // import logo from './logo.svg';
 import './App.css';
 import Login from "./pages/Login/Login";
-import { BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Home from './pages/Home/Home';
 import Register from './pages/Register/Register';
 import { useContext } from 'react';
 import { AuthContext } from './context/AuthContext';
 
 function App() {
-  const { user  } = useContext(AuthContext)
+  const { user } = useContext(AuthContext)
   return (
     <BrowserRouter>
-   <div>
-   <Routes>
-   <Route exact path="/" element={ user?    <Home/>  :<Register/>  }/>
-<Route exact path="/login" element={ user? <Navigate  to="/" />: <Login/>}/>
-<Route exact path="/register" element={ user? <Navigate to="/"/> :<Register/>}/>
+      <div>
+        <Routes>
+          <Route exact path="/" element={user ? <Home /> : <Register />} />
+          <Route exact path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+          <Route exact path="/register" element={user ? <Navigate to="/" /> : <Register />} />
 
-   </Routes>
-   </div>
-   </BrowserRouter>
-  // <Home/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+    // <Home/>
   );
 }
 
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,38 +1,38 @@
 import { useRef } from "react";
 import "./register.css";
 import axios from "axios";
-import { Link, useNavigate} from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 export default function Register() {
 
-  const  Navigate=  useNavigate();
+  const Navigate = useNavigate();
   const name = useRef()
   const username = useRef();
   const email = useRef();
   const password = useRef();
   const passwordAgain = useRef();
 
-  const handleClick= async (e)=>{
+  const handleClick = async (e) => {
     e.preventDefault();
-    if(password.current.value!==passwordAgain.current.value){
+    if (password.current.value !== passwordAgain.current.value) {
       passwordAgain.current.setCustomValidity("password Does not match. Please enter matched password.")
     }
     else {
-const user={
-    name : name.current.value,
-username: username.current.value,
-email:email.current.value,
-password:password.current.value,
-};
+      const user = {
+        name: name.current.value,
+        username: username.current.value,
+        email: email.current.value,
+        password: password.current.value,
+      };
 
-try {
-  await axios.post("http://localhost:4000/v1/auth/register",user)
-  Navigate("/login")
-} catch (error) {
-  console.log(error, "hellooo");
-}
+      try {
+        await axios.post("http://localhost:4000/v1/auth/register", user)
+        Navigate("/login")
+      } catch (error) {
+        console.log(error, "hellooo");
+      }
     };
-    }
+  }
 
   return (
     <div className="login">
@@ -47,14 +47,14 @@ try {
           <form className="loginBox" onSubmit={handleClick}>
             <input placeholder="name" className="loginInput" required ref={name} />
             <input placeholder="Username" className="loginInput" required ref={username} />
-            <input placeholder="Email"  type="email" className="loginInput" required ref={email} />
+            <input placeholder="Email" type="email" className="loginInput" required ref={email} />
             <input placeholder="Password" type="password" className="loginInput" required ref={password} />
             <input placeholder="Password Again" type="password" className="loginInput" required ref={passwordAgain} />
             <button className="loginButton" type="submit">Sign Up</button>
             <Link to="/login">
-            <button className="loginRegisterButton">
-              Log into Account
-            </button>
+              <button className="loginRegisterButton">
+                Log into Account
+              </button>
             </Link>
           </form>
         </div>
