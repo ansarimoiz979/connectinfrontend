3bfed86b15653323c3fb02c9cbd544e1143e6a1e
ansarimoiz979 <ansarimoiz979@gmail.com>
2023-04-15 16:16:38 +0530

    attach auth token in header globally when we got token in login api success response call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -5,7 +5,9 @@ export const loginCall = async (userCredential, dispatch) => {
   try {
     const res = await axios.post("http://localhost:4000/v1/auth/login"
     , userCredential);
-    // axios.defaults.headers.common["Authorization"] = res.data.accessToken;
+  // axios.defaults.baseURL = 'http://localhost:1010/'
+  axios.defaults.headers.common = {'Authorization': `bearer ${res.data.accessToken}`}
+  
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
   } catch (err) {
     dispatch({ type: "LOGIN_FAILURE", payload: err });
